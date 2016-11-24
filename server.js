a6c8cdc2f14fc8e5aa9d349c0b1a5b1647c60c12
spencerdezartsmith <spencer.dezart.smith@gmail.com>
2016-11-24 11:15:39 -0800

    Add port const to server.js

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require('express');
 
 const app = express();
+// setting a port key
+app.set('port', process.env.PORT || 3000);
+const port = app.get('port');
 
 app.get('/', (req, res, next) => {
   res.send('<h1>Hello Express</h1>');
 });
 
-app.listen(3000, () => console.log('Gurlwhereyouat is running on port 3000'));
+app.get('/dashboard', (req, res, next) => {
+  res.send('<h1>This is the dashboard');
+});
+
+app.listen(port, () => {
+  console.log('Gurlwhereyouat is listening on port:', port);
+});
