b82566dec528543a4bfd89a3397811ed2c9e292f
spencerdezartsmith <spencer.dezart.smith@gmail.com>
2016-11-24 10:07:16 -0800

    Set up the initial server with home route

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-console.log('Welcome to Gurlwhereyouat');
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res, next) => {
+  res.send('<h1>Hello Express</h1>');
+});
+
+app.listen(3000, () => console.log('Gurlwhereyouat is running on port 3000'));
