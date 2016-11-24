fc9010b51ed35a60fcd3c836fef87f7452bfbabc
spencerdezartsmith <spencer.dezart.smith@gmail.com>
2016-11-24 12:08:48 -0800

    Render login page

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 // setting a port key
@@ -6,7 +7,7 @@ app.set('port', process.env.PORT || 3000);
 const port = app.get('port');
 
 app.get('/', (req, res, next) => {
-  res.send('<h1>Hello Express</h1>');
+  res.sendFile(path.join(__dirname, '/views/login.htm'));
 });
 
 app.get('/dashboard', (req, res, next) => {
