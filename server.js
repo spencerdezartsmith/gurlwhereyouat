d1739144bf0b0a5e8c89164496c89421c6c4af8b
spencerdezartsmith <spencer.dezart.smith@gmail.com>
2016-11-24 13:22:26 -0800

    Remove the root route from server file

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const app = express();
+const chat = require('./app');
 
 // setting a port key
 app.set('port', process.env.PORT || 3000);
@@ -11,13 +12,9 @@ app.use(express.static('public'));
 // express will auto require ejs
 app.set('view engine', 'ejs');
 
-const port = app.get('port');
+app.use('/', chat.router);
 
-app.get('/', (req, res, next) => {
-  res.render('login', {
-    'pageTitle': 'My Login Page'
-  });
-});
+const port = app.get('port');
 
 app.get('/dashboard', (req, res, next) => {
   res.send('<h1>This is the dashboard');
