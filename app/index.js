dd6998df7d8929a6487783b1af4d587487a39a49
spencerdezartsmith <spencer.dezart.smith@gmail.com>
2016-11-25 14:59:15 -0800

    Add routes object

--- app/index.js
+++ app/index.js
@@ -2,8 +2,12 @@ const router = require('express').Router();
 
 router.get('/', (req, res, next) => {
   res.render('login', {
-    'pageTitle': 'My Login Page'
+    pageTitle: 'My Login Page'
   });
+});
+
+router.get('/info', (req, res, next) => {
+  res.send('test page');
 });
 
 module.exports = { router };
