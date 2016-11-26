b93b34dd8b2afb82596a0f74ce66ec57d9269693
spencerdezartsmith <spencer.dezart.smith@gmail.com>
2016-11-26 15:44:44 -0800

    Export sessions

---
+++ app/session/index.js
@@ -0,0 +1,23 @@
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const config = require('../config');
+const db = require('../db');
+
+if (process.env.NODE_ENV === 'production') {
+  // Initialize session with settings for production
+  module.exports = session({
+    secret: config.sessionSecret,
+    resave: false,
+    saveUnitialized: false,
+    store: new MongoStore({
+      mongooseConnection: db.mongoose.connection
+    })
+  });
+} else {
+  // Initialize session with settings for dev
+  module.exports = session({
+    secret: config.sessionSecret,
+    resave: false,
+    saveUnitialized: true
+  });
+}
