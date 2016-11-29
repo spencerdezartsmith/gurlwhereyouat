dcdc1a353a8ed30de203be9c1ae4c97143e78bd1
spencerdezartsmith <spencer.dezart.smith@gmail.com>
2016-11-29 10:40:21 -0800

    Require auth strategy

--- app/index.js
+++ app/index.js
@@ -2,6 +2,9 @@ const routes = require('./routes');
 const session = require('./session');
 require('dotenv').load();
 
+// Social Authentication Logic
+require('./auth')();
+
 module.exports = {
   router: routes(),
   session
