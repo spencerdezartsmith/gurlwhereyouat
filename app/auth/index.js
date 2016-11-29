f958c59879542ab66ddcc7990f2bdd5d4057ce5c
spencerdezartsmith <spencer.dezart.smith@gmail.com>
2016-11-29 10:39:09 -0800

    Complete facebook OAuth Strategy

--- app/auth/index.js
+++ app/auth/index.js
@@ -1,12 +1,36 @@
 const passport = require('passport');
 const config = require('../config');
 const FacebookStrategy = require('passport-facebook').Strategy;
+const helper = require('../helpers');
 
 module.exports = () => {
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser((id, done) => {
+    // Find user using the id
+    helper.findById(id)
+      .then(user => done(null, user))
+      .catch(error => console.log('Error when deserializing the user!'));
+  });
+
+
   let authProcessor = (accessToken, refreshToken, profile, done) => {
     // Find a user profile in the local db using profile id
     // If user is found, return the user data using the done()
     // If user is not found, create one in the local db and call
+    helper.findOne(profile.id)
+      .then(result => {
+        if (result) {
+          done(null, result);
+        } else {
+          // Create a new user and return
+          helper.createNewUser(profile)
+            .then(newChatUser => done(null, newChatUser))
+            .catch(error => console.log('Error when creating new user'));
+        }
+      });
   };
 
   passport.use(new FacebookStrategy(config.fb, authProcessor));
