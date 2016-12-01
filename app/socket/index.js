cced98d8bf6b68df772129cdb13bd0a71ad77cbe
spencerdezartsmith <spencer.dezart.smith@gmail.com>
2016-12-01 13:19:30 -0800

    Update the socket module to create new chat rooms

--- app/socket/index.js
+++ app/socket/index.js
@@ -1,7 +1,28 @@
+const helpers = require('../helpers');
+
 module.exports = (io, app) => {
   let allRooms = app.locals.chatrooms;
 
   io.of('/roomslist').on('connection', socket => {
-    console.log('Socket.io connected to client');
+    socket.on('getChatrooms', () => {
+      socket.emit('chatRoomsList', JSON.stringify(allRooms));
+    });
+
+    socket.on('createNewRoom', newRoomInput => {
+      // Check to see if a room already exists with that name
+      // If not, create the room and add it to the chatrooms array.
+      if (!helpers.findRoomByName(allRooms, newRoomInput)) {
+        allRooms.push({
+          room: newRoomInput,
+          roomID: helpers.randomHex(),
+          users: []
+        });
+
+        // Emit an updated list to the creator
+        socket.emit('chatRoomsList', JSON.stringify(allRooms));
+        // Emit an updated list to everyone connected to the rooms pageTitle
+        socket.broadcast.emit('chatRoomsList', JSON.stringify(allRooms));
+      }
+    });
   });
 };
