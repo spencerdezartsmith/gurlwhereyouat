a6a00919c016df038b1d59d7331804f027ee2b37
spencerdezartsmith <spencer.dezart.smith@gmail.com>
2016-12-02 14:51:49 -0800

    Add socket listener for a chatroom connection

--- app/socket/index.js
+++ app/socket/index.js
@@ -1,8 +1,8 @@
-const helpers = require('../helpers');
+const helper = require('../helpers');
 
 module.exports = (io, app) => {
-  let allRooms = app.locals.chatrooms;
-
+  const allRooms = app.locals.chatrooms;
+  // Listening for the roomslist namespace
   io.of('/roomslist').on('connection', socket => {
     socket.on('getChatrooms', () => {
       socket.emit('chatRoomsList', JSON.stringify(allRooms));
@@ -11,10 +11,10 @@ module.exports = (io, app) => {
     socket.on('createNewRoom', newRoomInput => {
       // Check to see if a room already exists with that name
       // If not, create the room and add it to the chatrooms array.
-      if (!helpers.findRoomByName(allRooms, newRoomInput)) {
+      if (!helper.findRoomByName(allRooms, newRoomInput)) {
         allRooms.push({
           room: newRoomInput,
-          roomID: helpers.randomHex(),
+          roomID: helper.randomHex(),
           users: []
         });
 
@@ -23,6 +23,16 @@ module.exports = (io, app) => {
         // Emit an updated list to everyone connected to the rooms pageTitle
         socket.broadcast.emit('chatRoomsList', JSON.stringify(allRooms));
       }
+    });
+  });
+  // Listening for the chatter namespace
+  io.of('/chatter').on('connection', socket => {
+    // Join chatroom
+    socket.on('join', data => {
+      const usersList = helper.addUserToRoom(allRooms, data, socket);
+
+      // Update the list of active users as shown on the chatroom page
+      console.log('usersList: ', usersList);
     });
   });
 };
